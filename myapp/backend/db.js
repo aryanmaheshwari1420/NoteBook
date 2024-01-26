5e647cdafa5174de5ad701bbb0a9ae1443c3c85b
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2024-01-27 01:05:46 +0530

    updation for deploy

--- myapp/backend/db.js
+++ myapp/backend/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/notebook";
+// const mongoURI = "mongodb://localhost:27017/notebook";
+const mongoURI = "mongodb+srv://aryanmaheshwari1420:notebookhaiye@cluster0.iyd729h.mongodb.net/notebook";
+
 
 const connectToMongo = async () => {
   try {
--- myapp/backend/index.js
+++ myapp/backend/index.js
@@ -4,7 +4,13 @@ var cors = require('cors')
 connectToMongo();
 const app = express()
 const port = 5000
-app.use(cors())
+app.use(cors(
+  {
+    origin:[""],
+    methods:['POST','GET','PUT','DELETE'],
+    credentials:true
+  }
+))
 app.use(express.json())
 
 // Available Routes
@@ -15,3 +21,5 @@ app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
   console.log(`Notebook is listening at http://localhost:${port}`)
 })
+
+// mongodb+srv://aryanmaheshwari1420:rr1234@cluster0.ef0pna7.mongodb.net/notebook
