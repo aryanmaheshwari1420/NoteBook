1d418f3e1f991ee4a8334f4e9c1bc5406b9cb1e5
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2024-01-28 00:42:02 +0530

    backend changes

--- myapp/backend/index.js
+++ myapp/backend/index.js
@@ -1,25 +1,23 @@
 const connectToMongo = require('./db');
 const express = require('express')
-var cors = require('cors')
+// var cors = require('cors')
 connectToMongo();
 const app = express()
 const port = 5000
-app.use(cors(
-  {
-    origin:[""],
-    methods:['POST','GET','PUT','DELETE'],
-    credentials:true
-  }
-))
+// app.use(cors(
+//   {
+//     origin:["https://note-book-live.vercel.app/login"],
+//     methods:['POST','GET','PUT','DELETE'],
+//     credentials:true
+//   }
+// ))
 app.use(express.json())
 
 // Available Routes
-app.use('/api/auth', require('./routes/auth'))
+app.use('/api/auth', require('./routes/auth'))  
 app.use('/api/notes', require('./routes/notes'))
 
 
 app.listen(port, () => {
   console.log(`Notebook is listening at http://localhost:${port}`)
 })
-
-// mongodb+srv://aryanmaheshwari1420:rr1234@cluster0.ef0pna7.mongodb.net/notebook
