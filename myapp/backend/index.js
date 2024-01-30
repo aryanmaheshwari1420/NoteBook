be40831489d43027fcbdd5a65d3f8b224062d1e1
Aryan Maheshwari <98485902+aryanmaheshwari1420@users.noreply.github.com>
2024-01-31 00:40:54 +0530

    Update index.js

--- myapp/backend/index.js
+++ myapp/backend/index.js
@@ -1,16 +1,10 @@
 const connectToMongo = require('./db');
 const express = require('express')
-// var cors = require('cors')
+var cors = require('cors')
 connectToMongo();
 const app = express()
 const port = 5000
-// app.use(cors(
-//   {
-//     origin:["https://note-book-live.vercel.app/login"],
-//     methods:['POST','GET','PUT','DELETE'],
-//     credentials:true
-//   }
-// ))
+app.use(cors());
 app.use(express.json())
 
 // Available Routes
